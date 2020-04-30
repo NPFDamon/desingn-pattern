8268aada6eda8b686604a28acc27a591d8d79528
Damon <554588461@qq.com>
2020-04-30 16:53:38 +0800

    组合模式

---
+++ src/main/java/com/cn/Composite/Node.java
@@ -0,0 +1,57 @@
+package com.cn.Composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+    private String leaf;
+    private String des;
+    private List<Node> nodes;
+
+    public Node(String leaf,String des){
+        this.leaf = leaf;
+        this.des = des;
+        nodes = new ArrayList<>();
+    }
+
+    public void add(Node node){
+        nodes.add(node);
+    }
+
+    public void remove(Node node){
+        nodes.remove(node);
+    }
+
+    public String getLeaf() {
+        return leaf;
+    }
+
+    public void setLeaf(String leaf) {
+        this.leaf = leaf;
+    }
+
+    public String getDes() {
+        return des;
+    }
+
+    public void setDes(String des) {
+        this.des = des;
+    }
+
+    public List<Node> getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(List<Node> nodes) {
+        this.nodes = nodes;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "leaf='" + leaf + '\'' +
+                ", des='" + des + '\'' +
+                ", nodes=" + nodes +
+                '}';
+    }
+}
---
+++ src/main/java/com/cn/Composite/Tree.java
@@ -0,0 +1,31 @@
+package com.cn.Composite;
+
+/***
+ * 组合模式；把一组相似的对象当做一个单一的对象。
+ * 将对象组合成树状结构表示“部分-整体”的结构层次。组合模式使得用户对单个对象和组合对象的使用具有一致性
+ */
+public class Tree {
+    public static void main(String[] args){
+        Node root = new Node("root","first");
+
+        Node secondLeft = new Node("left","second left");
+        Node secondRight = new Node("right","second right");
+
+        Node secondLeftLeft = new Node("left","second Left Left");
+        Node secondLeftRight = new Node("right","second Left Right");
+
+        Node secondRightLeft = new Node("left","second Right Left");
+        Node secondRightRight = new Node("right","second Right Right");
+
+        root.add(secondLeft);
+        root.add(secondRight);
+
+        secondLeft.add(secondLeftLeft);
+        secondLeft.add(secondLeftRight);
+
+        secondRight.add(secondRightLeft);
+        secondRight.add(secondRightRight);
+
+        System.out.println(root);
+    }
+}
